ded1f1c95c994d6cc5fa74363a01a4144a8dea7c
Aleksandrs Ulme <tw.at.mrk+au@gmail.com>
2014-10-31 14:27:49 +0000

    1st dojo

---
+++ NinetyNineBottlesOfBeer/Bottles.cs
@@ -0,0 +1,79 @@
+﻿namespace NinetyNineBottlesOfBeer
+{
+    public interface IVerseProvider
+    {
+        string GetVerse(int bottles);
+    }
+
+    public class MultipleUpperMultipleLowerVerseProvider : IVerseProvider
+    {
+        public string GetVerse(int bottles)
+        {
+            return string.Format("{0} bottles of beer on the wall, {0} bottles of beer.\r\nTake one down and pass it around, {1} bottles of beer on the wall.", bottles, bottles - 1);
+        }
+    }
+
+    public class MultipleUpperSingularLowerVerseProvider : IVerseProvider
+    {
+        public string GetVerse(int bottles)
+        {
+            return string.Format("{0} bottles of beer on the wall, {0} bottles of beer.\r\nTake one down and pass it around, {1} bottle of beer on the wall.", bottles, bottles - 1);
+        }
+    }
+
+    public class PenultimateVerseProvider : IVerseProvider
+    {
+        public string GetVerse(int bottles)
+        {
+            return string.Format("{0} bottle of beer on the wall, {0} bottle of beer.\r\nTake it down and pass it around, no more bottles of beer on the wall.", bottles);
+        }
+    }
+
+
+    public class LastVerseProvider : IVerseProvider
+    {
+        public string GetVerse(int bottles)
+        {
+            return
+                "No more bottles of beer on the wall, no more bottles of beer.\r\nGo to the store and buy some more, 99 bottles of beer on the wall.";
+        }
+    }
+
+    public class Bottles
+    {
+        public string Verse(int num)
+        {
+            var verseProvider = GetVerseProvider(num);
+            return verseProvider.GetVerse(num);
+        }
+
+        private IVerseProvider GetVerseProvider(int num)
+        {
+            if (num > 2)
+            {
+                return new MultipleUpperMultipleLowerVerseProvider();
+            }
+            else if (num == 2)
+            {
+                return new MultipleUpperSingularLowerVerseProvider();
+            }
+            else if (num == 1)
+            {
+                return new PenultimateVerseProvider();
+            }
+
+            return new LastVerseProvider();
+        }
+
+        public string Verse(int upperBound, int lowerBound)
+        {
+
+            string finalAnswer = "";
+            for (int i = upperBound; i >= lowerBound; i--)
+            {
+                finalAnswer += GetVerseProvider(i).GetVerse(i) + "\r\n\r\n";
+            }
+            return finalAnswer;
+        }
+    }
+}
---
+++ NinetyNineBottlesOfBeerTests/BottlesTest.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+using NinetyNineBottlesOfBeer;
+
+namespace NinetyNineBottlesOfBeerTests
+{
+    [TestFixture]
+    public class BottlesTest
+    {
+        private Bottles _bottles;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _bottles = new Bottles();
+        }
+
+        [Test]
+        public void first_verse()
+        {
+            const string expected = @"99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.";
+            Assert.That(_bottles.Verse(99), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void another_verse()
+        {
+            const string expected = @"89 bottles of beer on the wall, 89 bottles of beer.
+Take one down and pass it around, 88 bottles of beer on the wall.";
+            Assert.That(_bottles.Verse(89), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void verse_2()
+        {
+            const string expected = @"2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.";
+            Assert.That(_bottles.Verse(2), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void verse_1()
+        {
+            const string expected = @"1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.";
+            Assert.That(_bottles.Verse(1), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void verse_0()
+        {
+            const string expected = @"No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.";
+            Assert.That(expected, Is.EqualTo(_bottles.Verse(0)));
+        }
+
+        [Test]
+        public void a_couple_of_verses()
+        {
+            const string expected = @"99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.
+
+98 bottles of beer on the wall, 98 bottles of beer.
+Take one down and pass it around, 97 bottles of beer on the wall.
+
+";
+            Assert.That(_bottles.Verse(99, 98), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void a_few_verses()
+        {
+            const string expected = @"2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+
+1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.
+
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+
+";
+            Assert.That(_bottles.Verse(2, 0), Is.EqualTo(expected));
+        }
+    }
+}
